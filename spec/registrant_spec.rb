712375c98554887945d133120d5ec6f37243edca
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-08-31 15:31:18 -0600

    added determine if permit T or false / pass

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -23,4 +23,20 @@ it 'can have a permit' do
     expect(registrant.age).to eq(18)
     expect(registrant.permit).to eq(true)
 end
+
+it 'can determine if permit is T or F' do
+    registrant_1 = Registrant.new("Bruce", 18, true)
+    registrant_2 = Registrant.new('Penny', 15 )
+
+    expect(registrant_1).to be_an_instance_of(Registrant)
+    expect(registrant_2).to be_an_instance_of(Registrant)
+    expect(registrant_1.name).to eq("Bruce")
+    expect(registrant_1.age).to eq(18)
+    expect(registrant_1.permit?).to eq(true)
+    expect(registrant_2.name).to eq("Penny")
+    expect(registrant_2.age).to eq(15)
+    expect(registrant_2.permit?).to eq(false)
+
+end
+
 end
