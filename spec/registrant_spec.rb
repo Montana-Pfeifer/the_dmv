2c2fd09db7dcf5555667d9f15f3f9e612d8824f6
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-08-31 09:13:01 -0600

    2nd test green

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -3,7 +3,7 @@ require './lib/registrant' #Require registrant.rb
 RSpec.describe Registrant do
 
 it 'it exists' do
-    expect(registrant).be be_an_instance_of(Registrant)
+    registrant = Registrant.new
 end
 
 
