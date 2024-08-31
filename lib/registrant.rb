1e69f10b6303c45217ed0864a168217a115c0d54
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-08-31 09:12:09 -0600

    created registrant

---
+++ lib/registrant.rb
@@ -0,0 +1,16 @@
+class Registrant
+
+    # attr_reader :name,
+    #             :age,
+    #             :permit,
+    #             :license_data
+
+
+    # def initialize(name, age, permit, license_data)
+    #     @name = name
+    #     @age = age
+    #     @permit = permit
+    #     @license_data = license_data
+    # end
+
+end
