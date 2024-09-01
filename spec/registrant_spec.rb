b18f17ee0290f6c96fa5aad77e665ce8bc28ea6c
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-08-31 18:36:06 -0600

    tested license_data

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -39,4 +39,14 @@ it 'can determine if permit is T or F' do
 
 end
 
+it 'can read license data' do
+    registrant_1 = Registrant.new("Bruce", 18, true)
+
+    expect(registrant_1).to be_an_instance_of(Registrant)
+    expect(registrant_1.name).to eq("Bruce")
+    expect(registrant_1.age).to eq(18)
+    expect(registrant_1.permit?).to eq(true)
+    expect(registrant_1.license_data).to eq(:written=>false, :license=>false, :renew=>false)
+end
+
 end
