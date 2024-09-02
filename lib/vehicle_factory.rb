5141508e5e9ed0d22760d938941e351f83db8357
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-02 14:09:08 -0600

    changed vin and year to correct pry testing

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -2,8 +2,8 @@ class VehicleFactory
     def self.create_vehicles(data)
         data.map do |entry|
         Vehicle.new(
-            vin: entry[:vin],
-            year: entry[:year],
+            vin: entry[:vin_1_10],
+            year: entry[:model_year].to_i,
             make: entry[:make],
             model: entry[:model],
             engine: :ev
