70648e0f1591a4ae523dd7bfec91678cef8fbe4d
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-02 13:20:31 -0600

    created test for create_vehicles

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,7 +1,21 @@
+require 'spec_helper'
 require './lib/vehicle_factory'
+require './lib/vehicle'
 
-RSpec describe VehicleFactory do
-    it 'exists' do
-        vehicles = VehicleFactory.new
+RSpec.describe VehicleFactory do
+
+   describe '.create_vehicles' do
+        it 'can create vehicles' do
+        data = [{vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} ]
+
+        vehicles = VehicleFactory.create_vehicles(data)
+
+        expect(vehicles.length).to eq(1)
+        expect(vehicles[0].vin).to eq('987654321abcdefgh')
+        expect(vehicles[0].year).to eq(2019)
+        expect(vehicles[0].make).to eq('Chevrolet')
+        expect(vehicles[0].model).to eq('Bolt')
+        expect(vehicles[0].engine).to eq(:ev)
+        end
     end
 end
