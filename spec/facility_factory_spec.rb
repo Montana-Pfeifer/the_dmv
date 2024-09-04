f0a40df43ca409703cf713690119ba00e40bdc4d
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-03 22:38:07 -0600

    removed first test because other tests show facilities being created

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -3,22 +3,6 @@ require 'spec_helper'
 RSpec.describe FacilityFactory do
    let (:factory){FacilityFactory.new}
 
-    describe '.create_facilities' do
-         xit 'can create' do
-            
-            factory = FacilityFactory.new
-            facility = [{name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'}]
-            dmv_test = factory.create_facilities(facility)
-            
-            
-            expect(dmv_test.length).to eq(1)
-            expect(dmv_test[0].name).to eq('DMV Tremont Branch')
-            expect(dmv_test[0].address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
-            expect(dmv_test[0].phone).to eq('(720) 865-4600')
-            
-        end
-     end
-
     describe '#format_address_co' do
      it 'formats a Colorado address correctly' do
        entry = { address_li: "2855 Tremont Place", address__1: "Suite 118", city: "Denver", state: "CO", zip: "80205"}
@@ -48,5 +32,32 @@ RSpec.describe FacilityFactory do
         expect(facilities[1].phone).to eq("(720) 865-4600")
     end
     
+    describe '#format_address_ny' do
+     it 'formats a New York address correctly' do
+       entry = { street_address_line_1: "560 WARREN STREET", city: "HUDSON", state: "NY", zip_code: "12534"}
+ 
+       formatted_address = factory.send(:format_address_ny, entry)
+ 
+       expect(formatted_address).to eq("560 WARREN STREET, HUDSON, NY, 12534")
+     end
+   end
+   describe '.create_facilities_for_ny'
+     it 'can create a facility list for ny' do
+        ny_entries = [{office_name: "HUDSON", street_address_line_1: "560 WARREN STREET", city: "HUDSON", state: "NY", zip_code: "12534", public_phone_number: "5188283350"},
+        {office_name: "ROCHESTER DOWNTOWN", street_address_line_1: "200 E. MAIN STREET", city: "ROCHESTER", state: "NY", zip_code: "14604", public_phone_number:"5852594526"}
+        ]
+
+        facilities = factory.create_facilities(ny_entries, :ny)
+
+        expect(facilities.length).to eq(2)
+
+        expect(facilities[0].name).to eq("HUDSON")
+        expect(facilities[0].address).to eq("560 WARREN STREET, HUDSON, NY, 12534")
+        expect(facilities[0].phone).to eq("5188283350")
+
+        expect(facilities[1].name).to eq("ROCHESTER DOWNTOWN")
+        expect(facilities[1].address).to eq("200 E. MAIN STREET, ROCHESTER, NY, 14604")
+        expect(facilities[1].phone).to eq("5852594526")
+    end
  end
 
