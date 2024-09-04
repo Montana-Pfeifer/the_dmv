ecbcd791a69796dccb095b35606274308ec215ca
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-03 22:35:51 -0600

    added ny facility methods

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -3,8 +3,10 @@ class FacilityFactory
         case state
         when :co
             create_facilities_for_co(entries)
+        when :ny
+            create_facilities_for_ny(entries)
         else
-            raise "Unsuppourted state #{state}"
+            create_facilities(entries)
         end
     end
 
@@ -20,5 +22,19 @@ class FacilityFactory
 
     def format_address_co(entry)
         "#{entry[:address_li]}, #{entry[:address__1]}, #{entry[:city]}, #{entry[:state]}, #{entry[:zip]}"
+    end
+
+    def create_facilities_for_ny(entries)
+        entries.map do |entry|
+            Facility.new(
+                name: entry[:office_name],
+                address: format_address_ny(entry),
+                phone: entry[:public_phone_number]
+            )
+        end
+    end
+
+    def format_address_ny(entry)
+        "#{entry[:street_address_line_1]}, #{entry[:city]}, #{entry[:state]}, #{entry[:zip_code]}"
     end
 end
