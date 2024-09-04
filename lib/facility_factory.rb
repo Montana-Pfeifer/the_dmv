be4d8eea8bbab8884714aee9853abbd33bd17afc
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-03 21:26:57 -0600

    changed Facility factory to take in multiple state dmv data

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,18 +1,24 @@
 class FacilityFactory
-    def create_facilities(facility)
-        
-        facility.map do |entry|
+    def create_facilities(entries, state)
+        case state
+        when :co
+            create_facilities_for_co(entries)
+        else
+            raise "Unsuppourted state #{state}"
+        end
+    end
+
+    def create_facilities_for_co(entries)
+        entries.map do |entry|
             Facility.new(
-                name: entry[:name],
-                address: entry[:address],
+                name: entry[:dmv_office],
+                address: format_address_co(entry),
                 phone: entry[:phone],
                 )
         end
     end
 
-    # def self.co_dmv_office_locations
-    #     @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
-    #     create_facilities(co_dmv_office_locations)
-    # end
-
+    def format_address_co(entry)
+        "#{entry[:address_li]}, #{entry[:address__1]}, #{entry[:city]}, #{entry[:state]}, #{entry[:zip]}"
+    end
 end
